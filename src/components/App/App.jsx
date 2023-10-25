a8a4a49e419c5459e315b028781d17f667f4460a
Mykhailo Volontyr <boommmeran@gmail.com>
2023-10-25 20:59:36 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from 'components/Layout';
-import NotFound from 'pages/NotFound';
 
 const Home = lazy(() => import('pages/Home'));
 const Movies = lazy(() => import('pages/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const NotFound = lazy(() => import('pages/NotFound'));
 const Cast = lazy(() => import('components/Cast'));
 const Reviews = lazy(() => import('components/Reviews'));
 
